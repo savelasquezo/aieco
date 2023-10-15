912d9ee0e26b207b4527b4e9d00bb1dc323e4612
savelasquezo <savelasquezo@gmail.com>
2023-10-15 15:03:35 -0500

    Update Aieco-V2-0

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,6 +21,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Tailwind NPM Patch
+NPM_BIN_PATH = '/root/.nvm/versions/node/v18.18.2/bin/npm'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
